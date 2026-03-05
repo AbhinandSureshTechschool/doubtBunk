96059ef89956c092ab288d596908f17ae7fc9b8b
AbhinandSureshTechschool <abhinandtc.techschool@gmail.com>
2026-03-05 10:14:21 +0530

    update: minor change on db.js for vps deploy issue

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -1,22 +1,23 @@
 import mongoose from "mongoose";
 
 
-const MONOGDB_URL = process.env.MONGODB_URL;
 
-if(!MONOGDB_URL) {
-    throw new Error("Please defind mongodb url in .env.local")
-};
 
 let cached = global.mongoose;
 
-if(!cached) {
+if (!cached) {
     cached = global.mongoose = { conn: null, promise: null }
 };
 
 async function connectDB() {
-    if(cached.conn) return cached.conn;
+    const MONOGDB_URL = process.env.MONGODB_URL;
 
-    if(!cached.promise) {
+    if (!MONOGDB_URL) {
+        throw new Error("Please defind mongodb url in .env.local")
+    };
+    if (cached.conn) return cached.conn;
+
+    if (!cached.promise) {
         cached.promise = await mongoose.connect(MONOGDB_URL, {
             bufferCommands: false
         })
